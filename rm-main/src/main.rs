b66e22be83e20700783ac66914d5e4f5096b25ea
slundi <slundi@users.noreply.github.com>
2025-12-05 15:35:11 +0100

    feat: logging (#140)
    Ref: intuis#133
    
    Co-authored-by: slundi <slundi@ikmail.com>
    Co-authored-by: micielski <73398428+micielski@users.noreply.github.com>

--- rm-main/src/main.rs
+++ rm-main/src/main.rs
@@ -16,6 +16,16 @@ use tui::app::App;
 
 #[tokio::main()]
 async fn main() -> Result<()> {
+    // configure logger
+    if let Some(writer) = rm_config::logging::get_log_file() {
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::INFO)
+            .with_level(true)
+            .with_writer(writer)
+            .with_ansi(false)
+            .init();
+    }
+  
     color_eyre::config::HookBuilder::default()
         .issue_url(concat!(env!("CARGO_PKG_REPOSITORY"), "/issues/new"))
         .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
@@ -30,6 +40,7 @@ async fn main() -> Result<()> {
     if let Some(command) = args.command {
         cli::handle_command(command).await?;
     } else {
+        tracing::info!("Starting rustmission");
         run_tui().await?;
     }
 
